86a9b3b5eb05f364f38680b13a04c03e76dcfd50
zarahi0997 <palomashc0919@gmail.com>
2025-04-07 17:10:04 -0600

    Add files via upload

---
+++ zarahi.sql
@@ -0,0 +1,21 @@
+create table materias( 
+id int, 
+nombre varchar(30),
+alumno varchar(30), 
+calificacion int 
+) 
+
+
+insert into materias (id, nombre, alumno, calificacion) VALUES 
+(1,'matematicas','Zarahi',8), 
+(2, 'ingles','Zarahi',7),
+(3, 'lengua y comunicacion', 'Zarahi',8),
+(4, 'socio emocional', 'Zarahi',7), 
+(5, 'cultura digital', 'Zarahi',10), 
+(6, 'con. Energia','Zarahi',9),
+(7, 'ciencias sociales','Zarahi',0),
+(8, 'CSdSI', 'zarahi',8),
+(9, 'DSdSI', 'Zarahi', 6), 
+(10, 'ISdSI', 'Zarahi',9);
+ 
+
